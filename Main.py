3bd31a07ac8800120a4f2b475cb74b83d474e5ce
RonaldoM12 <rmedina1@umd.edu>
2025-08-03 19:36:51 -0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,7 +1,19 @@
+import analysis
+import Data
+import etl
+import vis
+"importing all of the folders"
+"in order to call upon the python files within"
+
+"""Main will be the main function that we call upon in order
+to run all of our code as well as periodically testing
+it"""
 def main():
     return 0
 
+
+"""the purpose of this code is to run and call upon our main
+funtion to execute """
 if __name__ == "__main__":
     main()
 
-    

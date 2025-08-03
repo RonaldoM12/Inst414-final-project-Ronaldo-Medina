eccf82d1ef02ceeb0857f49a658eb0ee8b92469b
RonaldoM12 <rmedina1@umd.edu>
2025-08-03 19:42:02 -0400

    Add files via upload

---
+++ analysis/evaluate.py
@@ -0,0 +1,5 @@
+"""
+In evaluate we are going to use metrics such as 
+accurace, and recall in order to get the model used to
+seeing potential scenarios
+"""
---
+++ analysis/model.py
@@ -0,0 +1,5 @@
+"""
+in model, we are going to design it with the intention
+of using Logistic Regression in order to compute all of the
+calculations
+"""
